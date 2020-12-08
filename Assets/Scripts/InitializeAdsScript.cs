2688a1750c299bbd3648d97ea237ac6f7c727226
Mateo <mateotomasgomez@gmail.com>
2020-12-08 13:52:59 +0100

    Initial commit

---
+++ Assets/Scripts/InitializeAdsScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class InitializeAdsScript : MonoBehaviour { 
+
+    string gameId = "1234567";
+    bool testMode = true;
+
+    void Start () {
+        Advertisement.Initialize (gameId, testMode);
+    }
+}
